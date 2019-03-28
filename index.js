82c5526329c270d8e4f544e58b612b3fbcf0e842
bipin21 <bipinkarki172@gmail.com>
2019-03-28 21:52:38 +0545

    Emitting messages

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ app.use(express.static('public'));
 var io=socket(server);
 io.on('connection',function(socket){
     console.log('made socket connection',socket.id);
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data);
+    });
 });
