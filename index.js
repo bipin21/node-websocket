0603cef7e3a34b044dc9edc87709e3d0a00deff0
bipin21 <bipinkarki172@gmail.com>
2019-03-30 15:43:19 +0545

    Broadcast messages

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var socket = require('socket.io');
 // App setup
 var app = express();
+// create server
 var server = app.listen(4000, function () {
     console.log('listening to requests on port 4000');
-
 });
 
 // static files
@@ -12,9 +12,13 @@ app.use(express.static('public'));
 
 //socket setup
 var io=socket(server);
+//server connection
 io.on('connection',function(socket){
     console.log('made socket connection',socket.id);
     socket.on('chat',function(data){
         io.sockets.emit('chat',data);
+    });
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
     });
 });
